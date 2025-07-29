290ac62b3d506e4f4663ecb54dba55e0f897606d
gorlepavan <2300030221@kluniversity.in>
2025-07-29 12:13:02 +0530

    hello world

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,10 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo" alt="Vite logo" />
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
+        <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
