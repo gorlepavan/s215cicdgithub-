62793295dd2921186e5b1db3bed9fcba276e9d51
gorlepavan <2300030221@kluniversity.in>
2025-07-29 12:42:38 +0530

    hello world

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/s215cicdgithub/",
+  base: "/s215cicdgithub-/",
   plugins: [react()],
 })
